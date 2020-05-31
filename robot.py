15c914d6d0282fd993273add51215f96aa7f1c11
Ernest Wong <sudo@ernestwong.nz>
2020-05-31 13:17:36 +1200

    Implement the robot calculations using numpy

--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ from brain import EvolvableBrain
 import random
 import simulation_state
 import math
+import numpy as np
 
 # arena_width
 # timestep
@@ -16,8 +17,10 @@ class Robot:
 		self.beta = 3.14159 / 4
 
 		#Robot's x, y position and heading angle
-		self.x = random.randrange(0, simulation_state.arena_width)
-		self.y = random.randrange(0, simulation_state.arena_width)
+		self.position = np.array([
+			random.randrange(0, simulation_state.arena_width),
+			random.randrange(0, simulation_state.arena_width)
+		])
 		self.a = random.randrange(0, 2.0*3.14159)
 
 		self.food_battery = 1.0
@@ -34,7 +37,7 @@ class Robot:
 
 		self.is_alive = True
 
-		self.brain = Brain()
+		self.brain = EvolvableBrain()
 		self.env = Environment()
 		self.sensor_values = tuple()
 
@@ -57,13 +60,13 @@ class Robot:
 		max_speed = 10.0
 
 		if self.is_alive:
-			self.x += simulation_state.timestep * cos(a) * (self.l_motor + self.r_motor) * max_speed
-			self.y += simulation_state.timestep * sin(a) * (self.l_motor + self.r_motor) * max_speed
+			self.x += simulation_state.timestep * np.cos(self.a) * (self.l_motor + self.r_motor) * max_speed
+			self.y += simulation_state.timestep * np.sin(self.a) * (self.l_motor + self.r_motor) * max_speed
 			self.a += simulation_state.timestep * max_speed * (self.l_motor - self.r_motor) / (2.0 * self.radius)
 
 			self.env.interact_with_robot()
-			self.food_battery = constrain(self.food_battery - 0.04 * simulation_state.timestep, 0.0, 1.0)
-			self.water_battery = constrain(self.food_battery - 0.04 * simulation_state.timestep, 0.0, 1.0)
+			self.food_battery = np.clip(self.food_battery - 0.04 * simulation_state.timestep, 0.0, 1.0)
+			self.water_battery = np.clip(self.food_battery - 0.04 * simulation_state.timestep, 0.0, 1.0)
 
 		if self.food_battery == 0.0 or self.water_battery == 0.0:
 			self.is_alive = False
@@ -71,41 +74,41 @@ class Robot:
 			self.water_battery = 0.0
 
 	def sense(self, sense_vector, thing):
-		k = 1.0
-		x = sense_vector[0]
-		y = sense_vector[1]
+		sensor_to_thing = thing - (self.position + sense_vector * self.radius)
+		sensor_to_thing_magnitude = np.linalg.norm(sensor_to_thing)
+		impact = (simulation_state.arena_width - sensor_to_thing_magnitude) / simulation_state.arena_width
+		attenuation = np.dot(sense_vector, sensor_to_thing / sensor_to_thing_magnitude)
 
-		sensor_to_thing = tuple(thing.x - (self.x + x * self.radius), t.y - (self.y + y * self.radius))
-		impact = (simulation_state.arena_width - sensor_to_thing.mag()) / simulation_state.arena_width
+		# TODO (Ernest): Why is it cubed?
 		attenuation *= attenuation * attenuation
+
 		if attenuation < 0.0:
 			attenuation = 0.0
 		return impact * attenuation
 
 	def calculate_change(self):
-		sense_vectors = []
-		sense_vectors.append([a - beta])
-		sense_vectors.append([a + beta])
+		sense_vectors = [np.array([np.cos(self.a - self.beta), np.sin(self.a - self.beta)]),
+						 np.array([np.cos(self.a + self.beta), np.sin(self.a + self.beta)])]
 
 		raw_sensor_value = 0.0
 		for i in range(0, len(sense_vectors) + 1):
 			raw_sensor_value = 0.0
 			for thing in self.env.foods:
-				s = sense(sense_vectors[i], thing)
+				s = self.sense(sense_vectors[i], thing)
 				if s > raw_sensor_value:
 					raw_sensor_value = s
-			sense_values[0][i] = raw_sensor_value
+			self.sense_values[0][i] = raw_sensor_value
 
 			raw_sensor_value = 0.0
 			for thing in self.env.waters:
-				s = sense(sense_vectors[i], thing)
+				s = self.sense(sense_vectors[i], thing)
 				if s > raw_sensor_value:
 					raw_sensor_value = s
-			sensor_values[1][i] = raw_sensor_value
+			self.sensor_values[1][i] = raw_sensor_value
 
 			raw_sensor_value = 0.0
 			for thing in self.env.traps:
-				s = sense(sense_vectors[i], thing)
+				s = self.sense(sense_vectors[i], thing)
 				if s > raw_sensor_value:
 					raw_sensor_value += s
-			sense_values[2][i] = raw_sensor_value
+			self.sense_values[2][i] = raw_sensor_value
