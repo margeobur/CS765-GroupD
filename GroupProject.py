d66d6980d33875968bc264634a271b80f370b327
Ernest Wong <sudo@ernestwong.nz>
2020-05-31 14:37:19 +1200

    Moved more global state to simulation_state & removed saving tracking

--- GroupProject.py
+++ GroupProject.py
@@ -1,30 +1,9 @@
-import random
-from robot import Robot
-from environment import Environment
-from brain import EvolvableBrain
-
-
-def setup():
-    pop_size = 30
-    env = Environment()
-    evolvable_brains = []
-    robots = []
-
-    for i in range(0, pop_size):
-        b = EvolvableBrain()
-        b.randomise()
-        evolvable_brains.append(b)
-
-    for i in range(0, pop_size):
-        r = Robot()
-        r.set_brain(evolvable_brains[i])
-        r.set_environment(env)
-        robots.append(r)
+import simulation_state
 
 
 def main():
     # user_input = input("Press Enter to start")
-    setup()
+    simulation_state.setup()
 
 
 main()
--- simulation_state.py
+++ simulation_state.py
@@ -1,16 +1,33 @@
-rxs = []
-rys = []
-ras = []
+from robot import Robot
+from environment import Environment
+from brain import EvolvableBrain
 
-food_xs = []
-food_ys = []
-water_xs = []
-water_ys = []
-trap_xs = []
-trap_ys = []
 
 aspect_ratio_x = 2.0/3.0
 text_size = 20
 arena_width = 40
 timestep = 0.04
 iteration = 0
+
+pop_size = 30
+env = None
+evolvable_brains = []
+robots = []
+
+
+def setup():
+    global env
+    global iteration
+    iteration = 0
+    env = Environment()
+
+    for i in range(0, pop_size):
+        b = EvolvableBrain()
+        b.randomise()
+        evolvable_brains.append(b)
+
+    for i in range(0, pop_size):
+        r = Robot()
+        r.set_brain(evolvable_brains[i])
+        r.set_environment(env)
+        robots.append(r)
