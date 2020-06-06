eb1f0172be938fda960783da00f04c2e18a7d58a
Ernest Wong <sudo@ernestwong.nz>
2020-06-06 22:23:18 +1200

    Fix brain using wrong sensor values for wrong mappings

--- brain.py
+++ brain.py
@@ -5,12 +5,13 @@ from genome import Laterality
 
 class Mapping:
     def __init__(self, gene):
-        self.n_points = len(gene.list)
-        self.xs = np.array([point.x.value for point in gene.list])
-        self.ys = np.array([point.y.value for point in gene.list])
+        self.n_points = len(gene.mapping.list)
+        self.side_weighting = np.array([1, 0] if gene.motor_side.laterality == Laterality.LEFT else [0, 1])
+        self.xs = np.array([point.x.value for point in gene.mapping.list])
+        self.ys = np.array([point.y.value for point in gene.mapping.list])
 
     def f(self, x):
-        return np.interp(x, self.xs, self.ys)
+        return np.interp(x, self.xs, self.ys) * self.side_weighting
 
     def draw(self):
         for i in range(0, self.n_points - 1):
@@ -23,19 +24,12 @@ class Mapping:
 
 class EvolvableBrain:
     def __init__(self, genome):
-        self.left_mappings = [Mapping(gene.mapping) for gene in genome.sensors.list
-                              if gene.motor_side.laterality == Laterality.LEFT]
-        self.right_mappings = [Mapping(gene.mapping) for gene in genome.sensors.list
-                               if gene.motor_side.laterality == Laterality.RIGHT]
+        self.mappings = [Mapping(gene) for gene in genome.sensors.list]
 
     def iterate(self, sensor_values):
-        lm_accum = 0
-        rm_accum = 0
+        accumulative_motor_power = [0, 0]
 
-        for mapping, sensor_value in zip(self.left_mappings, sensor_values):
-            lm_accum += mapping.f(sensor_value)
+        for mapping, sensor_value in zip(self.mappings, sensor_values):
+            accumulative_motor_power += mapping.f(sensor_value)
 
-        for mapping, sensor_value in zip(self.right_mappings, sensor_values):
-            rm_accum += mapping.f(sensor_value)
-
-        return lm_accum / 6.0, rm_accum / 6.0
+        return accumulative_motor_power / 6.0
