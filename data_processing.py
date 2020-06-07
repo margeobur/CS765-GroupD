bd290f0aa9a0d74ce7ed3fb9cb7f023a741612ba
Ernest Wong <sudo@ernestwong.nz>
2020-06-07 20:00:08 +1200

    Add number of sensors and things to plot

--- data_processing.py
+++ data_processing.py
@@ -2,23 +2,42 @@ import os
 import json
 import matplotlib.pyplot as plt
 import statistics
+import re
+import itertools
+
 
 #SET NUMBER OF TOURNAMENTS
-number_of_tournaments = 10
+number_of_tournaments = 2761
 peak_fitnesses = [0] * number_of_tournaments
 mean_robot_fitnesses = [0] * number_of_tournaments
 mean_environment_fitnesses = [0] * number_of_tournaments
 peak_population_robot_fitnesses = [0] * number_of_tournaments
 peak_population_environment_fitnesses = [0] * number_of_tournaments
+total_thing_amount = [0] * number_of_tournaments
+total_sensor_amount = [0] * number_of_tournaments
 
 for filename in os.listdir("Data"):
     path = os.path.join("Data", filename)
     with open(path, "r") as json_file:
-        data = json.load(json_file)
+        try:
+            data = json.load(json_file)
+        except:
+            break
 
     if "population_data" in filename:
         environment_genomes = data["environment_genomes"]
         robot_genomes = data["robot_genomes"]
+        tournament = int(re.findall("(\\d+)\\.", filename)[0])
+        if tournament < number_of_tournaments:
+            total_thing_amount[tournament] = 0
+            for _, environment in data["environment_genomes"].items():
+                for thing in itertools.chain(environment["water_genes"], environment["food_genes"], environment["trap_genes"]):
+                    total_thing_amount[tournament] += thing["amount"]
+            total_thing_amount[tournament] /= len(data["environment_genomes"])
+            total_sensor_amount[tournament] = 0
+            for _, robot in data["robot_genomes"].items():
+                total_sensor_amount[tournament] += len(robot["sensors"])
+            total_sensor_amount[tournament] /= len(data["robot_genomes"])
 
     elif "tournament_data" in filename:
         tournament = data["tournament"]
@@ -39,43 +58,57 @@ for filename in os.listdir("Data"):
 
 #graph peak fitness over tournaments
 plt.figure(0)
-plt.xlabel("number of tournaments")
-plt.ylabel("peak_fitness")
+plt.ylabel("number of tournaments")
+plt.xlabel("peak_fitness")
 plt.title("peak fitness over tournaments")
 plt.plot(range(1, number_of_tournaments + 1), peak_fitnesses)
 plt.savefig("Graphs/peak_fitness_over_tournaments.png")
 
 #graph robot fitnesses for each tournament
 plt.figure(1)
-plt.xlabel("mean robot fitness for the population")
-plt.ylabel("tournament")
+plt.ylabel("mean robot fitness for the population")
+plt.xlabel("tournament")
 plt.title("mean robot fitness for the population over each tournament")
 plt.plot(range(1, number_of_tournaments + 1), mean_robot_fitnesses)
 plt.savefig("Graphs/mean_robot_fitness.png")
 
 #graph of peak robot fitness for the population for each tournament
 plt.figure(2)
-plt.xlabel("peak robot fitness for the population")
-plt.ylabel("tournament")
+plt.ylabel("peak robot fitness for the population")
+plt.xlabel("tournament")
 plt.title("peak robot fitness for the population over each tournament")
 plt.plot(range(1, number_of_tournaments + 1), peak_population_robot_fitnesses)
 plt.savefig("Graphs/peak_robot_fitness_for_population.png")
 
 #graph of peak environment fitness for the population fitness for each tournament
 plt.figure(3)
-plt.xlabel("peak robot fitness for the population")
-plt.ylabel("tournament")
+plt.ylabel("peak robot fitness for the population")
+plt.xlabel("tournament")
 plt.title("peak robot fitness for the population over each tournament")
 plt.plot(range(1, number_of_tournaments + 1), peak_population_environment_fitnesses)
 plt.savefig("Graphs/peak_environment_fitness_for_population.png")
 
 #graph of mean environment fitness for each tournament
 plt.figure(4)
-plt.xlabel("peak environment fitness")
-plt.ylabel("tournament")
+plt.ylabel("peak environment fitness")
+plt.xlabel("tournament")
 plt.title("mean environment fitness for each tournament")
 plt.plot(range(1, number_of_tournaments + 1), mean_environment_fitnesses)
 plt.savefig("Graphs/mean_environment_fitness_for_each_tournament.png")
+
+plt.figure(5)
+plt.ylabel("amount of things")
+plt.xlabel("tournament")
+plt.title("mean number of things for each tournament")
+plt.plot(range(1, number_of_tournaments + 1), total_thing_amount)
+plt.savefig("Graphs/mean_number_of_things_for_each_tournament.png")
+
+plt.figure(6)
+plt.ylabel("amount of sensors")
+plt.xlabel("tournament")
+plt.title("mean number of sensors for each tournament")
+plt.plot(range(1, number_of_tournaments + 1), total_sensor_amount)
+plt.savefig("Graphs/mean_number_of_sensors_for_each_tournament.png")
 
 
 
